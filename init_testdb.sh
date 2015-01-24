69bb1ed20b0d96c16fec974e3fd66f7bd5e061cc
Albert Sun <atschx@gmail.com>
2015-01-24 18:40:10 +0800

    special fileds

--- init_testdb.sh
+++ init_testdb.sh
@@ -6,10 +6,10 @@ echo $MYSQL
 $MYSQL test <<EOF
 drop table if exists user;
 create table user(id int,name varchar(30))ENGINE=InnoDB DEFAULT CHARSET=utf8;
-insert into user values (1,'张三');
-insert into user values (2,'李四');
-insert into user values (3,'王五');
-insert into user values (4,'赵六');
+insert into user (id,name) values (1,'张三');
+insert into user (id,name) values (2,'李四');
+insert into user (id,name) values (3,'王五');
+insert into user (id,name) values (4,'赵六');
 set character_set_results=utf8;
 select * from user;
 EOF
